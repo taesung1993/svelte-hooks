cf98224ba2438cb27beb6e3dbe3b10cc2241b7ef
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-06-28 19:08:49 +0900

    add global setting

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang=ts context=module>
+    import type {Load} from '@sveltejs/kit';
+    export const load: Load = async ({ stuff
+, url}) => {
+        return {}
+    }
+</script>
+
+<script lang=ts>
+
+</script>
+
+<slot></slot>
+
+<style lang=scss>
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :global(html) {
+        font-size: 10px;
+    }
+</style>
