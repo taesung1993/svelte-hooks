626e4520b80f638ec6b9deccc88afa8a71ff6b51
cheonyulin@gmail.com <mason@gollala.com>
2022-07-18 18:04:06 +0900

    add toast store

--- src/lib/store/toast.ts
+++ src/lib/store/toast.ts
@@ -2,17 +2,26 @@ import { writable } from "svelte/store";
 
 function createToasts() {
     const {subscribe, set, update} = writable<any>([]);
+    let index = 1;
     
-    function openToast(message: string, type: string) {
-        return update((toast: any) => {
-            toast = [...toast, {message, type}];
-            console.log(toast);
-            return toast;
+    function openToast(message: string, type?: string) {
+        return update((toasts: any) => {
+            const id = `toast-${index++}`;
+            toasts = [...toasts, {id, message, type}];
+            return toasts;
+        });
+    }
+
+    function closeToast(id: string) {
+        return update((toasts: any) => {
+            toasts = toasts.filter((toast: any) => toast.id !== id);
+            return toasts;
         });
     }
 
     return {
       open: openToast,
+      close: closeToast,
       subscribe
     };
 }
